043659507eb97690944da2d213ae7eee8e997ef8
Mehrnaz Shams <mshamsabadi78@gmail.com>
2021-10-24 19:52:43 +0330

    Add test for Owner

--- src/test/java/org/springframework/samples/petclinic/owner/OwnerTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/OwnerTest.java
@@ -1,7 +1,30 @@
 package org.springframework.samples.petclinic.owner;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 class OwnerTest {
 
+	// state verification
+	@Test
+	public void Pet_is_added_correctly() {
+		Owner owner = new Owner();
+		Pet pet = mock(Pet.class);
+		given(pet.isNew()).willReturn(true);
+		owner.addPet(pet);
+		assertEquals(owner.getPets().size(), 1);
+	}
+
+	// behavior verification
+	@Test
+	public void Pet_is_not_added_but_set_owner() {
+		Owner owner = new Owner();
+		Pet pet = mock(Pet.class);
+		given(pet.isNew()).willReturn(false);
+		owner.addPet(pet);
+		verify(pet, times(1)).setOwner(owner);
+	}
 }
