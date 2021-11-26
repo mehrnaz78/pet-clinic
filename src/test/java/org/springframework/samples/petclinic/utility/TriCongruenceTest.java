691dce8e452ed7be3165ef75128af9cfb8ab268e
Mehrnaz Shams <mshamsabadi78@gmail.com>
2021-11-26 17:30:35 +0330

    ca4

--- src/test/java/org/springframework/samples/petclinic/utility/TriCongruenceTest.java
+++ src/test/java/org/springframework/samples/petclinic/utility/TriCongruenceTest.java
@@ -9,6 +9,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @ExtendWith(ReportingExtension.class)
+@ClauseDefinition(clause = 'a', def = "t1arr[0] != t2arr[0]")
+@ClauseDefinition(clause = 'b', def = "t1arr[1] != t2arr[1]")
+@ClauseDefinition(clause = 'c', def = "t1arr[2] != t2arr[2]")
+@ClauseDefinition(clause = 'd', def = "t1arr[0] < 0")
+@ClauseDefinition(clause = 'e', def = "t1arr[0] + t1arr[1] < t1arr[2]")
 class TriCongruenceTest {
 
 	private static final Logger log = LoggerFactory.getLogger(TriCongruenceTest.class);
@@ -22,10 +27,143 @@ class TriCongruenceTest {
 		Assertions.assertFalse(areCongruent);
 	}
 
-	/**
-	 * TODO
-	 * explain your answer here
-	 */
+	@UniqueTruePoint(
+		predicate = "a+b+c",
+		dnf = "a+b+c",
+		implicant = "a",
+		valuations = {
+			@Valuation(clause = 'a', valuation = false),
+			@Valuation(clause = 'b', valuation = false),
+			@Valuation(clause = 'c', valuation = false),
+		}
+	)
+	@Test
+	public void questionOne_CUTPNFP_FFF() {
+		Triangle t1 = new Triangle(2, 3, 4);
+		Triangle t2 = new Triangle(2, 3, 4);
+		boolean areCongruent = TriCongruence.areCongruent(t1, t2);
+		log.debug("Triangles identified as '{}'.", areCongruent ? "Congruent" : "Not Congruent");
+		Assertions.assertTrue(areCongruent);
+	}
+
+	@UniqueTruePoint(
+		predicate = "a+b+c",
+		dnf = "a+b+c",
+		implicant = "a",
+		valuations = {
+			@Valuation(clause = 'a', valuation = true),
+			@Valuation(clause = 'b', valuation = false),
+			@Valuation(clause = 'c', valuation = false),
+		}
+	)
+	@Test
+	public void questionOne_CUTPNFP_TFF() {
+		Triangle t1 = new Triangle(2, 3, 4);
+		Triangle t2 = new Triangle(1, 3, 4);
+		boolean areCongruent = TriCongruence.areCongruent(t1, t2);
+		log.debug("Triangles identified as '{}'.", areCongruent ? "Congruent" : "Not Congruent");
+		Assertions.assertFalse(areCongruent);
+	}
+
+	@UniqueTruePoint(
+		predicate = "a+b+c",
+		dnf = "a+b+c",
+		implicant = "b",
+		valuations = {
+			@Valuation(clause = 'a', valuation = false),
+			@Valuation(clause = 'b', valuation = true),
+			@Valuation(clause = 'c', valuation = false),
+		}
+	)
+	@Test
+	public void questionOne_CUTPNFP_FTF() {
+		Triangle t1 = new Triangle(2, 3, 5);
+		Triangle t2 = new Triangle(2, 4, 5);
+		boolean areCongruent = TriCongruence.areCongruent(t1, t2);
+		log.debug("Triangles identified as '{}'.", areCongruent ? "Congruent" : "Not Congruent");
+		Assertions.assertFalse(areCongruent);
+	}
+
+	@UniqueTruePoint(
+		predicate = "a+b+c",
+		dnf = "a+b+c",
+		implicant = "c",
+		valuations = {
+			@Valuation(clause = 'a', valuation = false),
+			@Valuation(clause = 'b', valuation = false),
+			@Valuation(clause = 'c', valuation = true),
+		}
+	)
+	@Test
+	public void questionOne_CUTPNFP_FFT() {
+		Triangle t1 = new Triangle(2, 3, 5);
+		Triangle t2 = new Triangle(2, 3, 4);
+		boolean areCongruent = TriCongruence.areCongruent(t1, t2);
+		log.debug("Triangles identified as '{}'.", areCongruent ? "Congruent" : "Not Congruent");
+		Assertions.assertFalse(areCongruent);
+	}
+
+
+	@ClauseCoverage(
+		predicate = "d+e",
+		valuations = {
+			@Valuation(clause = 'd', valuation = true),
+			@Valuation(clause = 'e', valuation = true)
+		}
+	)
+	@Test
+	public void questionOne_CC_TT() {
+		Triangle t1 = new Triangle(-1, 3, 6);
+		boolean areCongruent = TriCongruence.areCongruent(t1, t1);
+		log.debug("Triangles identified as '{}'.", areCongruent ? "Congruent" : "Not Congruent");
+		Assertions.assertFalse(areCongruent);
+	}
+
+//	CC
+//	CACC
+	@ClauseCoverage(
+		predicate = "d+e",
+		valuations = {
+			@Valuation(clause = 'd', valuation = false),
+			@Valuation(clause = 'e', valuation = false)
+		}
+	)
+	@Test
+	public void questionOne_CC_FF() {
+		Triangle t1 = new Triangle(2, 3, 4);
+		boolean areCongruent = TriCongruence.areCongruent(t1, t1);
+		log.debug("Triangles identified as '{}'.", areCongruent ? "Congruent" : "Not Congruent");
+		Assertions.assertTrue(areCongruent);
+	}
+
+	@ClauseCoverage(
+		predicate = "d+e",
+		valuations = {
+			@Valuation(clause = 'd', valuation = false),
+			@Valuation(clause = 'e', valuation = true)
+		}
+	)
+	@Test
+	public void questionOne_CACC_FT() {
+		Triangle t1 = new Triangle(2, 3, 6);
+		boolean areCongruent = TriCongruence.areCongruent(t1, t1);
+		log.debug("Triangles identified as '{}'.", areCongruent ? "Congruent" : "Not Congruent");
+		Assertions.assertFalse(areCongruent);
+	}
+
+	/*
+		f = ab + c
+			UPTC:
+				implicant: ab, c
+					ab: {TTF}
+					c: {FTT, FFT, TFT}
+			CUTPNFP:
+				implicant: ab, c
+					{TTF, FFT, 	FFF, TFF, FTF}
+
+			UPTC covers	FTT, TFT cases but CUTPNFP doesn't.
+	*/
+
 	private static boolean questionTwo(boolean a, boolean b, boolean c, boolean d, boolean e) {
 		boolean predicate = false;
 //		predicate = a predicate with any number of clauses
